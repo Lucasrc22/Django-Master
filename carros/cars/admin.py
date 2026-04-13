f13384135e2254c1d7322eaee80ad71a714ea70b
Lucasrc22 <lucasrios2210@gmail.com>
2026-04-13 09:43:29 -0300

    criando models de carros para o banco de dados, e listando campos ao admin

--- carros/cars/admin.py
+++ carros/cars/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from cars.models import Car
+
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value') #listar os campos na tabela do admin
+    search_fields = ('model',) # vai poder buscar o modelo criado
+
+admin.site.register(Car, CarAdmin) #registrando a classe criada no admin
+
--- carros/cars/models.py
+++ carros/cars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+#Criará as tabelas para o banco de dados
 
-# Create your models here.
+class Car(models.Model): #Nome da classe que criará na tabela do banco de dados
+    id = models.AutoField(primary_key=True) #Campo de identificação único para cada carro, chave primaria do banco de dados
+    model = models.CharField(max_length=200) # Campo de texto, max_lenght-max de caracters
+    brand = models.CharField(max_length=200) #Marca
+    factory_year = models.IntegerField(blank=True, null=True) #numeros inteiros
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True) #numero decimal
+    #blank=True, null=True torna obrigatório valor
+
+#atualizar o banco python manage.py makemigrations, python manage.py migrate
+
